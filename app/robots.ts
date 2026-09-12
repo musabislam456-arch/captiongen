75456dbefc111d98f2edbf5fa9dce4c755f46f8a
Musab Bin Umair <musabislam456@gmail.com>
2026-09-12 16:59:36 +0500

    fix: correct production domain (captiongen.utilix.site)

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://captiongen.app/sitemap.xml',
+    sitemap: 'https://captiongen.utilix.site/sitemap.xml',
   };
 }
